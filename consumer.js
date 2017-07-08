7ba5c46ea4466138af43c6079cf1c02f475c6f30
Sämir Hamadi <samir.hamadi@gmail.com>
2017-07-08 16:53:36 +0300

    Possibility to get all shards or from iterator

--- consumer.js
+++ consumer.js
@@ -31,23 +31,39 @@ function printRecords(err, recordsData) {
     }
 }
 
-function findInitialShardIterator(err, shardIteratorData, func) {
+function findIntitalIterator(err, shardIteratorData, func) {
     if (err) console.log(err);
     else {
-        console.log(shardIteratorData);
-        kinesis.getRecords({
-            ShardIterator: shardIteratorData.ShardIterator
-        }, func);
+        kinesis.getRecords(constructShardIterator(shardIteratorData.ShardIterator), func);
     }
 }
 
-function fromBeginning(err, streamData, func) {
+function constructShardIterator(shardIterator) {
+    return {
+        ShardIterator: shardIterator
+    };
+}
+
+function allShardsFromBeginning(err, streamData, func) {
+    if (err) console.log(err);
+    else {
+        streamData.StreamDescription.Shards.forEach(shard => {
+            kinesis.getShardIterator(constructShardParams(shard.ShardId), (err, streamData) =>
+                findIntitalIterator(err, streamData, func));
+        });
+    }
+}
+
+function fromShardIterator(err, streamData, shardIterator, func) {
     if (err) console.log(err);
     else {
         streamData.StreamDescription.Shards.forEach(shard =>
-            kinesis.getShardIterator(constructShardParams(shard.ShardId), (err, streamData) => findInitialShardIterator(err,streamData, func)));
+            kinesis.getShardIterator(constructShardParams(shard.ShardId), (err, streamData) =>
+                kinesis.getRecords(constructShardIterator(shardIterator), func)));
     }
 }
 
-kinesis.describeStream(params, (err, streamData) => fromBeginning(err, streamData, printRecords));
+kinesis.describeStream(params, (err, streamData) => allShardsFromBeginning(err, streamData, printRecords));
+kinesis.describeStream(params, (err, streamData) => fromShardIterator(err, streamData, "AAAAAAAAAAFTa27KTq7ZzuKIsAu3YhjtS8Mu6/6+2RfWYyiUW2B1FuwxsWu8xtBJS82UqOm2cgmtR69kpp9fz8N2DwUykasdCQCcDsmf3MeYz4Hm8aiA52iZOWFO9FfgnfhZ2TGnmrwDETE94GBEuEgRqOHVsbU1lIMx/3Wx9mRqRBqazbn+SnuV1OREF0vm6xw8KjyZaLnqGkqqQeSrH8V7L5kK3Vjd", printRecords));
+
 
